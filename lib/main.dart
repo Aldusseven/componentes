cdcff447b51a529654a4422d862efd744bde6154
Aldusseven <200028@utags.edu.mx>
2023-03-30 10:21:45 -0600

    tercer commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:componentes/ui/screens/alerts_screen.dart';
-import 'package:componentes/ui/screens/home_page_temp.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:componentes/ui/screens/screens.dart';
+
 
 void main() => runApp(const MyApp());
 
@@ -12,18 +13,45 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'App Componentes',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [
+         //Locale('en', 'US'), //para inglés americano
+         Locale('es', 'MX'), //para español mexicano
+
+      ],
       //home: HomePageTemp(),
       initialRoute: '/',
       routes: <String, WidgetBuilder>{
         '/': (BuildContext context) => const HomePageTemp(),
         'alert':(BuildContext context) => const AlertsScreen(),
+        'inputs': (BuildContext context) => const InputsScreen(),
+        //crear entrada un nuevo menu
       },
       onGenerateRoute: (RouteSettings settings) {
         print('Ruta llamada: ${settings.name}');
         return MaterialPageRoute(
           builder: (BuildContext context) => const AlertsScreen());
 
-      }
-    );
+      },
+      theme: ThemeData.dark().copyWith(
+        //color primario
+        primaryColor: Colors.indigo,// asugnará color a los iconos de la app
+        //AppBar theme
+        appBarTheme: const AppBarTheme(
+          color: Colors.blueGrey, 
+          elevation: 0,
+        )
+
+
+      ) //propiedad de tema oscuro para toda la aplicación
+
+
+
+
+      );
+  
   }
 }
---
+++ lib/ui/screens/screens.dart
@@ -0,0 +1,5 @@
+export 'package:componentes/ui/screens/alerts_screen.dart';
+export 'package:componentes/ui/screens/home_page_temp.dart';
+export 'package:componentes/ui/screens/inputs_screen.dart';
+
+//crear el export necesario para la biblioteca se exporte en caso de necesitar un nuevo menu
